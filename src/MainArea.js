053745b64df2b1fb3f673722485093808506a7eb
Ettore Noviello <5858408+Santya95@users.noreply.github.com>
2025-02-03 19:03:03 +0100

    fix

--- src/MainArea.js
+++ src/MainArea.js
@@ -40,9 +40,9 @@ const MainArea = () => {
     );
 
     const renderMainContent = () => (
-        <div className="flex flex-column align-items-center justify-content-center h-screen fadein animation-duration-500 h-screen" style={{ backgroundImage: 'url(/3.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className="flex flex-column align-items-center justify-content-center h-screen fadein animation-duration-500" style={{ backgroundImage: 'url(/3.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            {renderTopBar && <TopBar renderComponent={renderComponentCallback} componentOnScreen={renderComponent} />}
             <BlockUI blocked={blocked} className='mt-8' fullScreen template={<i className="pi pi-spin pi-spinner" style={{ fontSize: '4rem', color: 'var(--primary-color)' }}></i>}>
-                {renderTopBar && <TopBar renderComponent={renderComponentCallback} componentOnScreen={renderComponent} />}
                 {ComponentOnScreen && <ComponentOnScreen renderComponent={renderComponentCallback} blockUi={blockUiCallaback} />}
             </BlockUI >
         </div >
